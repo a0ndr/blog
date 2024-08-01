e6764ffc5ba04484717b847cf478d7edde56b753
Adam Ondrejcak <adam@ondrejcak.sk>
2024-08-01 17:27:01 +0200

    added credits on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,3 +13,7 @@
 <div class="prose !max-w-none text-{alignment} text-gray-300 prose-headings:text-gray-300 prose-a:text-gray-200 prose-a:underline">
     {@html converter.makeHtml(data.texts.find((x: {id: string, content: string}) => x.id === "ABOUT_US").content)}
 </div>
+
+<hr class="w-48 border-t-2 border-slate-700 mx-auto my-5">
+
+<p class="text-slate-500">Made with ♥︎ by Fuzzy! ∙ Source code on <a href="https://github.com/a0ndr/blog" class="underline" target="_blank">GitHub</a> ∙ Licensed under GNU GPLv3</p>
