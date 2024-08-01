fa714ec2451adb3b2764df48fd04d7e4e72774c5
Adam Ondrejcak <adam@ondrejcak.sk>
2024-08-01 16:59:42 +0200

    make the link color brighter in /about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,6 +10,6 @@
     const alignment = data.config.find((x: {key: string, value: string}) => x.key === "ABOUT_TEXT_ALIGNMENT").value;
 </script>
 
-<div class="prose !max-w-none text-{alignment} text-gray-300 prose-headings:text-gray-300 prose-a:text-gray-400 prose-a:underline">
+<div class="prose !max-w-none text-{alignment} text-gray-300 prose-headings:text-gray-300 prose-a:text-gray-200 prose-a:underline">
     {@html converter.makeHtml(data.texts.find((x: {id: string, content: string}) => x.id === "ABOUT_US").content)}
 </div>
